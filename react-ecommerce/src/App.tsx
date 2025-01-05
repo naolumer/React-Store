fd04a82d60f8288b0cb409688a6b4a3e0fd720d4
naolumer <naolumer3@gmail.com>
2025-01-05 23:48:48 +0300

    add filtering functionality

--- react-ecommerce/src/App.tsx
+++ react-ecommerce/src/App.tsx
@@ -1,12 +1,19 @@
+import MainContent from "./components/MainContent"
 import Sidebar from "./components/Sidebar"
-import { BrowserRouter as Router } from "react-router-dom"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 
 export default function App() {
   return (
     <Router>
       <div className="flex h-screen">
         <Sidebar/>
+        <div className="rounded w-full flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent/>}/>
+          </Routes>
+        </div>
       </div>
+      
         
     </Router>
   )
---
+++ react-ecommerce/src/components/MainContent.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import { useFilter } from './FilterContext'
+import { Tally3 } from 'lucide-react';
+import axios from 'axios';
+
+function MainContent() {
+    const {searchQuery, selectedCategory, minPrice, maxPrice, keyword} =useFilter();
+    const [products,setProducts] = useState<any[]>([])
+    const [filter, setFilter] = useState("all")
+    const [currentPage,setCurrentPage] = useState(1)
+    const [dropdownOpen, setDropdownOpen] = useState(true)
+    const itemsPerPage = 12
+
+    useEffect(()=>{
+        let url = `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${(currentPage-1)*itemsPerPage}`
+
+        if (keyword){
+            url = `https://dummyjson.com/products/search?q=${keyword}`
+        }
+        axios.get(url).then(response => {
+            setProducts(response.data.products)
+            console.log(response.data.products)
+        }).catch(error=> {
+            console.error("Error fetching the data",error )
+        })
+
+    },[currentPage,keyword])
+
+
+    const getFilteredProducts = ()=> {
+        let filteredProducts = products
+
+        if (selectedCategory) {
+           filteredProducts = filteredProducts.filter(
+            (product)=>product.category===selectedCategory);
+        };
+
+        if (minPrice!==undefined) {
+            filteredProducts = filteredProducts.filter(product=> product.price >= minPrice)
+        }
+
+        if (maxPrice!==undefined) {
+            filteredProducts = filteredProducts.filter(product=> product.price <= maxPrice)
+        }
+
+        if (searchQuery) {
+            filteredProducts = filteredProducts.filter(product=>
+             product.title.toLowerCase().includes(searchQuery.toLowerCase()))
+        };
+
+        switch (filter) {
+            case "expensive":
+                return filteredProducts.sort((a,b)=> b.price-a.price)
+            case "cheap":
+                return filteredProducts.sort((a,b)=> a.price - b.price)
+            case "popular":
+                return filteredProducts.sort((a,b)=> b.rating - a.rating)
+            default:
+                return filteredProducts;
+        }
+
+
+    }
+
+    const filteredProducts = getFilteredProducts();
+    console.log(filteredProducts)
+
+  return (
+    <section className='xl:w-[55rem] lg:w-[55rem] sm:w[40rem] xs:w=[20rem] p-5'>
+        <div className='mb-5'>
+            <div className='flex flex-col sm:flex-row justify-between items-center'>
+                <div className='relative mb-5 mt-5'>
+                    <button type='button' className='border px-4 py-2 rounded-full flex items-center'>
+                        <Tally3 className='mr-2'/>
+
+                        {filter==="all"? "Filter":filter.charAt(0).toLowerCase() + filter.slice(1)}
+                    </button>
+                {dropdownOpen && (
+                    <div className='absolute bg-white border-gray-300 rounded mt-2 w-full sm:w-40'>
+                        <button
+                            type='button' 
+                            onClick={()=>setFilter("cheap")} 
+                            className='block px-4 py-2 w-full text-left hover:bg-gray-200'
+                        >
+                        Cheap
+                        
+                        </button>
+
+                        <button
+                            type='button' 
+                            onClick={()=>setFilter("expensive")} 
+                            className='block px-4 py-2 w-full text-left hover:bg-gray-200'
+                        >
+                        Expensive
+                        
+                        </button>
+
+                        <button
+                            type='button' 
+                            onClick={()=>setFilter("popular")} 
+                            className='block px-4 py-2 w-full text-left hover:bg-gray-200'
+                        >
+                        Popular
+                        
+                        </button>
+
+                    </div>
+                )}
+                </div>
+            </div>
+
+            <div className='grid grid-cols-4 sm:grid-cols-3 md:grid-cols-4 gap-5'>
+                {/* BookCard */}
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default MainContent
